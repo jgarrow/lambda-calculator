470eabb81d7e2145020cc30775102af66dd744a3
Janessa Garrow <janessa.garrow@gmail.com>
2019-11-14 16:49:37 -0700

    Added all button components to App.js

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const SpecialButton = () => {
+export default ({ special }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{special}</button>
     </>
   );
 };
