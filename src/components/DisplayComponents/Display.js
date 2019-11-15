583b7f9a6db7f150ac7ec6d7f48938e77fad8bb7
Janessa Garrow <janessa.garrow@gmail.com>
2019-11-14 18:45:20 -0700

    Imported all components to App.js. Completed MVP

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+export default ({ result }) => {
+  return (
+    <div className="display">
+      {/* Display any props data here */}
+      {result}
+    </div>
+  );
 };
